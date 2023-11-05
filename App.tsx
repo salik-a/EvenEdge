fe0f1f1409a0c3625935be5a8ee156607d481b26
salik-a <salika@mef.edu.tr>
2023-11-06 01:26:58 +0300

    paper provider added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { PaperProvider } from 'react-native-paper';
 import RootNavigation from './src/route'
 function App(): JSX.Element {
 
   return (
     <SafeAreaProvider>
-      <RootNavigation />
+      <PaperProvider>
+        <RootNavigation />
+      </PaperProvider>
     </SafeAreaProvider>
   );
 }
