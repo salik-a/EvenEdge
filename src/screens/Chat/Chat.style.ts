96e3f188fef3630c75b3e7b6157688344c030fe8
salik-a <salika@mef.edu.tr>
2023-11-06 04:23:57 +0300

    chat screen design update and api call added for open ai

--- src/screens/Chat/Chat.style.ts
+++ src/screens/Chat/Chat.style.ts
@@ -1,9 +1,33 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  messageContainer: {
+    borderTopEndRadius: 20,
+    borderTopStartRadius: 20,
+    borderBottomStartRadius: 20,
+    borderWidth: 0.4,
+    backgroundColor: '#00000005',
+    maxWidth: '75%',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    alignSelf: 'flex-end',
+    marginRight: 10,
+  },
+  messageText: {
+    fontSize: 16,
+    color: 'black',
+  },
+  gptMessageContainer: {
+    alignSelf: 'flex-start',
+    marginTop: 20,
+    borderBottomStartRadius: 0,
+    borderBottomEndRadius: 20,
+    marginLeft: 10,
+  },
 });
 
 export default styles;
--- src/screens/Chat/Chat.tsx
+++ src/screens/Chat/Chat.tsx
@@ -1,13 +1,63 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useCallback, useMemo, useState } from 'react';
+import { Image, Text, View } from 'react-native';
 import styles from './Chat.style';
+import Header from '../../components/Header';
+import axios from 'axios';
+import { useFocusEffect } from '@react-navigation/native';
+import { apiKey } from '../../utils/constants';
 
-const Chat: React.FC = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Chat Screen</Text>
-        </View>
-    )
+const Chat: React.FC = ({ route }) => {
+  const message = useMemo(() => route.params.message, [route]);
+  const [gptMessage, setGptMessage] = useState('');
+  const handleSubmit = async () => {
+    const client = axios.create({
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + apiKey,
+        'OpenAI-Organization': 'org-OPKit4SghBGYNqyRm0pi8Emt',
+      },
+    });
+    const response = await client.post(
+      'https://api.openai.com/v1/completions',
+      {
+        prompt: message,
+        temperature: 0.7,
+        max_tokens: 150,
+        model: 'text-davinci-003',
+      },
+    );
+    setGptMessage(response.data.choices[0].text);
+  };
+  useFocusEffect(
+    useCallback(() => {
+      handleSubmit();
+    }, []),
+  );
+  return (
+    <View style={styles.container}>
+      <Header
+        title={'Bored AI'}
+        rightIcon={
+          <Image
+            style={{
+              width: 25,
+              height: 25,
+            }}
+            source={require('../../assets/icAvatar.png')}
+          />
+        }
+        showGoBack
+      />
+      <View style={styles.messageContainer}>
+        <Text style={styles.messageText}>{message}</Text>
+      </View>
+      <View style={[styles.messageContainer, styles.gptMessageContainer]}>
+        <Text style={styles.messageText}>
+          {gptMessage === '' ? 'Thinking...' : gptMessage}
+        </Text>
+      </View>
+    </View>
+  );
 };
 
 export default Chat;
