c182d8e51fc7a7f46c082ddb4e3eeb91f5b3447d
salik-a <salika@mef.edu.tr>
2023-11-06 07:10:34 +0300

    header part and delete alert modal added to history screen

--- src/screens/History/History.style.ts
+++ src/screens/History/History.style.ts
@@ -1,9 +1,58 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
+  container: {
+    flex: 1,
+  },
+  headerCancel: {
+    fontSize: 18,
+    marginLeft: 4,
+  },
+  headerImage: {
+    width: 25,
+    height: 25,
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    width: '90%',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    paddingVertical: 20,
+  },
+  trashImage: {
+    width: 50,
+    height: 50,
+  },
+  deleteTitle: {
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 18,
+  },
+  deleteText: {
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  randomizeButton: {
+    width: '40%',
+    alignSelf: 'center',
+    borderColor: 'black',
+    borderRadius: 6,
+  },
+  findButton: {
+    width: '40%',
+    alignSelf: 'center',
+    borderColor: 'black',
+    borderRadius: 6,
+    backgroundColor: 'black',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    marginTop: 20,
+    width: '100%',
+    justifyContent: 'space-around',
+  },
 });
 
 export default styles;
--- src/screens/History/History.tsx
+++ src/screens/History/History.tsx
@@ -1,13 +1,112 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useCallback, useState, useRef } from 'react';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import styles from './History.style';
+import Header from '../../components/Header';
+import { TextInput, Portal, Modal, Button } from 'react-native-paper';
 
 const History: React.FC = () => {
+  const [data, setData] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const inputRef = useRef(null);
+
+  const [visible, setVisible] = React.useState(false);
+
+  const showModal = useCallback(() => setVisible(true), []);
+  const hideModal = useCallback(() => setVisible(false), []);
+
+  const handleDelete = useCallback(() => {
+    hideModal();
+  }, [hideModal]);
+
+  const HeaderCenter = useCallback(() => {
     return (
-        <View style={styles.container}>
-            <Text>History Screen</Text>
-        </View>
-    )
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TextInput
+          ref={inputRef}
+          placeholder="Search"
+          mode="outlined"
+          value={data}
+          onChangeText={(text) => setData(text)}
+          left={<TextInput.Icon icon="text-search" />}
+          right={
+            data !== '' ? (
+              <TextInput.Icon
+                onPress={() => {
+                  inputRef.current.blur();
+                  setIsFocused(false);
+                  setData('');
+                }}
+                icon="close-circle"
+              />
+            ) : null
+          }
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          style={{
+            backgroundColor: 'white',
+            borderRadius: 20,
+            width: isFocused ? '80%' : '99%',
+          }}
+          outlineStyle={{ borderRadius: 50 }}
+          outlineColor="#00000050"
+          activeOutlineColor="black"
+        />
+        {isFocused && (
+          <TouchableOpacity>
+            <Text style={styles.headerCancel}>Cancel</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  }, [data, setData, isFocused, setIsFocused]);
+  return (
+    <View style={styles.container}>
+      <Header
+        title={'Bored AI'}
+        rightIcon={
+          <Image
+            style={styles.headerImage}
+            source={require('../../assets/trash.png')}
+          />
+        }
+        rightIconPress={showModal}
+        centerComponent={<HeaderCenter />}
+      />
+      <Text>History Screen</Text>
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          contentContainerStyle={styles.modalContent}>
+          <Image
+            style={styles.trashImage}
+            source={require('../../assets/trash.png')}
+          />
+          <Text style={styles.deleteTitle}>Delete History</Text>
+          <Text style={styles.deleteText}>
+            Are you sure to remove all history items, this action is
+            irreversible!
+          </Text>
+          <View style={styles.modalButtons}>
+            <Button
+              mode="outlined"
+              style={styles.randomizeButton}
+              textColor="black"
+              onPress={hideModal}>
+              Cancel
+            </Button>
+            <Button
+              mode="contained"
+              style={styles.findButton}
+              textColor="white"
+              onPress={handleDelete}>
+              Delete
+            </Button>
+          </View>
+        </Modal>
+      </Portal>
+    </View>
+  );
 };
 
 export default History;
