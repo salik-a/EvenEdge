beba1a42a6a2b2590e7625708875eaec90eb6f92
salik-a <salika@mef.edu.tr>
2023-11-06 23:21:43 +0300

    set gpt message to local data functions added

--- src/screens/Chat/Chat.tsx
+++ src/screens/Chat/Chat.tsx
@@ -5,10 +5,12 @@ import Header from '../../components/Header';
 import axios from 'axios';
 import { useFocusEffect } from '@react-navigation/native';
 import { apiKey } from '../../utils/constants';
+import { storage } from '../../services/storage';
 
 const Chat: React.FC = ({ route }) => {
-  const message = useMemo(() => route.params.message, [route]);
+  const item = useMemo(() => route.params.item, [route]);
   const [gptMessage, setGptMessage] = useState('');
+
   const handleSubmit = async () => {
     const client = axios.create({
       headers: {
@@ -17,20 +19,34 @@ const Chat: React.FC = ({ route }) => {
         'OpenAI-Organization': 'org-OPKit4SghBGYNqyRm0pi8Emt',
       },
     });
-    const response = await client.post(
-      'https://api.openai.com/v1/completions',
-      {
-        prompt: message,
+    await client
+      .post('https://api.openai.com/v1/completions', {
+        prompt: item.activity,
         temperature: 0.7,
         max_tokens: 150,
         model: 'text-davinci-003',
-      },
-    );
-    setGptMessage(response.data.choices[0].text);
+      })
+      .then((res) => {
+        setGptMessage(res.data.choices[0].text);
+        const activitiesGetArray = storage.getString('activitiesArray');
+        const activitiesArray = activitiesGetArray
+          ? JSON.parse(activitiesGetArray)
+          : [];
+        const serializedArray = JSON.stringify([
+          ...activitiesArray,
+          { ...item, gptMessage: res.data.choices[0].text, date: Date.now() },
+        ]);
+        storage.set('activitiesArray', serializedArray);
+      })
+      .catch((err) => console.log(err));
   };
   useFocusEffect(
     useCallback(() => {
-      handleSubmit();
+      if (item.sendRequest) {
+        handleSubmit();
+      } else {
+        setGptMessage(item.gptMessage);
+      }
     }, []),
   );
   return (
@@ -49,7 +65,7 @@ const Chat: React.FC = ({ route }) => {
         showGoBack
       />
       <View style={styles.messageContainer}>
-        <Text style={styles.messageText}>{message}</Text>
+        <Text style={styles.messageText}>{item.activity}</Text>
       </View>
       <View style={[styles.messageContainer, styles.gptMessageContainer]}>
         <Text style={styles.messageText}>
